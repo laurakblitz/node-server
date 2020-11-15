77c95f2b176ea5cefcb9b14fb772fc5dd2b6585c
Laura Blitz <laura.k.blitz@gmail.com>
2020-11-15 15:31:56 -0500

    Updates to app.js

--- server/app.js
+++ server/app.js
@@ -1,25 +1,47 @@
 require('dotenv').config();
-let express = require('express'); 
-let app = express(); 
+let express = require('express');
+let app = express();
 let sequelize = require('./db');
 
+let journal = require("./controllers/journalcontroller");
 let user = require('./controllers/usercontroller');
-let journal = require('./controllers/journalcontroller');
 
 sequelize.sync();
+//sequelize.sync({force: true})
 
-app.use(require('./middleware/headers'));
+app.use(require('./middleware/headers'))
 
 app.use(express.json());
 
-// *** EXPOSED ROUTE *** //
 app.use('/user', user);
-
-// *** PROTECTED ROUTE *** //
-app.use(require('./middleware/validate-session'));
 app.use('/journal', journal);
 
-
 app.listen(3000, function(){
-    console.log('App is listening on port 3000');
+    console.log("App is listening on port 3000")
 });
+
+// require('dotenv').config();
+// let express = require('express'); 
+// let app = express(); 
+// let sequelize = require('./db');
+
+// let user = require('./controllers/usercontroller');
+// let journal = require('./controllers/journalcontroller');
+
+// sequelize.sync();
+
+// app.use(require('./middleware/headers'));
+
+// app.use(express.json());
+
+// // *** EXPOSED ROUTE *** //
+// app.use('/user', user);
+
+// // *** PROTECTED ROUTE *** //
+// app.use(require('./middleware/validate-session'));
+// app.use('/journal', journal);
+
+
+// app.listen(3000, function(){
+//     console.log('App is listening on port 3000');
+// });
