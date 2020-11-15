5bff053eac462aa549aea987fb0e257c89db73ef
Laura Blitz <laura.k.blitz@gmail.com>
2020-11-15 13:57:29 -0500

    Updates

--- client/user-scripts.js
+++ client/user-scripts.js
@@ -74,6 +74,23 @@ function userLogout() {
 *** TOKEN CHECKER FUNCTION ***
 ************************** */
 function tokenChecker() {
-     console.log('tokenChecker Function Called')
+    
+    let display = document.getElementById('journals')
+    let header = document.createElement('h5')
+    let accessToken = localStorage.getItem('SessionToken')
+    let text = 'Login or signup to get started!'
+
+    for (i = 0; i = display.childNodes.length; i++) {
+        display.removeChild(display.firstChild)
     }
-//     tokenChecker()
+
+    if (accessToken === 'undefined') {
+        display.appendChild(header);
+        header.textContent = text
+        header.setAttribute('id', 'defaultLogin');
+    } else {
+        null
+    }
+
+}
+tokenChecker()
